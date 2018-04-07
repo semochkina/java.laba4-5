31cb368c94505c8f1bfced70986620e8e1b87998
Semochkina <sveta_sem_1@mail.ru>
2018-04-07 22:36:14 +0600

    add file to laba4-5

---
+++ src/test/java/FunOneArgumentExpTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Тест для функции вида f(x) = Aexp(x) + B
+ */
+public class FunOneArgumentExpTest {
+    /** погрешность для сравнения вещественных чисел */
+    private static final double DELTA = 0.0001;
+
+    @Test
+    public void test() {
+        FunOneArgumentExp fun = new FunOneArgumentExp(1, 3, 3, 2);
+        System.out.println(fun.calculate(2));
+        Assert.assertEquals(fun.calculate(2), 24.16716829679195, DELTA);
+        Assert.assertEquals(fun.segmentBegin(), 1., DELTA);
+        Assert.assertEquals(fun.segmentEnd(), 3., DELTA);
+    }
+}
